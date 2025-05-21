2a2a608d640724d772cbb23afb08d19e3ed2919c
Facundo Nostas <155680624+FacundoN100@users.noreply.github.com>
2025-05-21 04:20:00 -0300

    Update Character.cs

--- Assets/1_ServiceAccess/Scripts/Characters/Character.cs
+++ Assets/1_ServiceAccess/Scripts/Characters/Character.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Excercise1
@@ -10,11 +9,25 @@ namespace Excercise1
         protected virtual void OnEnable()
         {
             //TODO: Add to CharacterService. The id should be the given serialized field. 
+            if (CharacterService.Instance == null)
+            {
+                Debug.LogError($"[Character] CharacterService singleton no existe en escena!");
+                return;
+            }
+            if (!CharacterService.Instance.TryAddCharacter(id, this))
+            {
+                Debug.LogWarning($"[Character] Ya existía un personaje con id='{id}'.");
+            }
         }
+        
 
         protected virtual void OnDisable()
         {
             //TODO: Remove from CharacterService.
+            if (CharacterService.Instance != null)
+            {
+                CharacterService.Instance.TryRemoveCharacter(id);
+            }
         }
     }
 }
